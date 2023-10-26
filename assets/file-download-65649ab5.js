0a03a5848bea0d62fa7442d8e9cad99625609a3b
Bruno Heitzmann Almada <brunoalmada1@hotmail.com>
2023-10-26 11:21:16 -0300

    66th build

--- assets/file-download-65649ab5.js
+++ assets/file-download-65649ab5.js
@@ -1,4 +1,4 @@
-import{H as vt,I as zt}from"./index-3ef68e52.js";function yt(ct){throw new Error('Could not dynamically require "'+ct+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var Ct={exports:{}};/*!
+import{H as vt,I as zt}from"./index-ce51e11d.js";function yt(ct){throw new Error('Could not dynamically require "'+ct+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var Ct={exports:{}};/*!
 
 JSZip v3.10.1 - A JavaScript class for generating and reading zip files
 <http://stuartk.com/jszip>
